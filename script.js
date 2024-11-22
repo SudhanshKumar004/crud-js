19cf03716c9258e97614cbe8723cde92c1e276b3
Sudhansh Kumar <sudhanshkb04@gmail.com>
2024-11-22 12:28:16 +0530

    added update button

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ async function show()
             <td>${e.age}</td>
             <td>${e.city}</td>
             <td><button onclick="mydelete('${e.id}')">delete</button></td>
+            <td><button onclick="myedit('${e.id}')">Edit</button></td>
         </tr>
     `).join("  ")
 
@@ -32,4 +33,54 @@ function mydelete(id)
         method:'DELETE'
     })
     .then(re=>alert("Deleted successfully.!!!"))
+}
+
+function insertdata()
+{
+    let data={
+        name : document.querySelector('#name').value,
+        age : document.querySelector('#age').value,
+        city : document.querySelector('#city').value
+    }
+    fetch("http://localhost:3000/student",{
+        method: 'POST',
+        Headers:{
+            'content-type': 'application/json'
+        },
+        body:JSON.stringify(data)
+    })
+    .then(res=>alert("inserted..!!!"))
+}
+
+ async function myedit(id){
+    let myupdate = await fetch(`http://localhost:3000/student/${id}`);
+    let redata = await myupdate.json()
+    let senddata = `
+    <h1>Updata Data</h1>
+    <input type="text" value="${redata.id}" readonly> <br>
+    <input type="text" value="${redata.name}" id="name1"> <br>
+    <input type="text" value="${redata.age}" id="age1"> <br>
+    <input type="text" value="${redata.city}" id="city1"> <br>
+    <input type="submit" onclick="finalupdate('${redata.id}')">
+    `
+
+    document.querySelector('#edittable').innerHTML = senddata
+}
+
+function finalupdate(id)
+{
+    let fdata = {
+        name:document.querySelector('#name1').value,
+        age:document.querySelector('#age1').value,
+        city:document.querySelector('#city1').value,
+    }
+    fetch(`http://localhost:3000/student/${id}`,
+    {
+        method:'PUT',
+        Headers:{
+            'content-type':'application/json'
+        },
+        body:JSON.stringify(fdata)
+    })
+    .then(r=>alert("Updated...!!"))
 }
